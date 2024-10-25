047794f3e264c77d71cbd31d138bbe131ea15dcb
Skylar Zhang <skylartianyuan@gmail.com>
2024-10-25 14:35:50 +0100

    Adapted function to merge data from different sheets and write them in a csv file.

--- datamerger.py
+++ datamerger.py
@@ -2,11 +2,12 @@ import os
 import pandas as pd
  
 # Check data schema from the MX deposit
-class Schemabuilder:
+class SchemaBuilder:
     def __init__(self, schema_folder, data_folder):
         self.schema_folder = schema_folder
         self.data_folder = data_folder
         self.schema = self.load_schema()
+        self.common_key = self.find_common_key()
  
     def load_schema(self):
         schema = {}
@@ -22,22 +23,34 @@ class Schemabuilder:
         lists = list(schema.values())
          # Find the intersection of all lists
         common_items = set(lists[0]).intersection(*lists)
-        return list(common_items)[0]
+        return list(common_items)[0] if common_items else None
  
     def merge_data(self):
         common_key= self.find_common_key()
+        if not common_key:
+            raise ValueError("No common key found in the schema files.")
+        
         merged_data = pd.DataFrame()
-        for file in os.listdir(self.data_folder):
-            if file.endswith('.xlsx'):
-                file_path = os.path.join(self.data_folder, file)
-                xls = pd.ExcelFile(file_path)
-                for sheet_name in xls.sheet_names:
-                    sheet_data = pd.read_excel(xls, sheet_name=sheet_name)
-                    if merged_data.empty:
-                        merged_data = sheet_data
-                    else:
-                        merged_data = pd.merge(merged_data, sheet_data, on=common_key, how='outer')
+        for filefolder in os.listdir(self.data_folder):
+            file_path = os.path.join(self.data_folder,filefolder)
+            if os.path.isdir(file_path):  # Check if it's a directory
+                for table in os.listdir(file_path):
+                    if table.endswith('.xlsx'):
+                        table_path = os.path.join(file_path, table)
+                        xls = pd.ExcelFile(table_path)
+                        for sheet_name in xls.sheet_names:
+                            sheet_data = pd.read_excel(xls, sheet_name=sheet_name)
+                            if merged_data.empty:
+                                merged_data = sheet_data
+                            else:
+                                merged_data = pd.merge(merged_data, sheet_data, on=common_key, how='outer')
+                                
+                # Write the merged data to a CSV file in the original Excel folder
+                output_file_path = os.path.join(file_path, 'merged_data.csv')
+                merged_data.to_csv(output_file_path, index=False)
+                print(f"Merged data written to {output_file_path}")
         return merged_data
+            
  
     def save_merged_data(self, output_file, common_key):
         merged_data = self.merge_data(common_key)
